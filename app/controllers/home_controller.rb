200ba2c8ef65209be248c62b7810d0edac4acd46
wheel0012 <wheel0012@outlook.kr>
2019-07-30 17:35:06 +0000

    add calc

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
 	def home
 	end
+	def calc
+		
+	end
+	def plus
+		@result = params[:param1].to_i + params[:param2].to_i
+	end
+	def result
+		@result = params[:param1].to_i + params[:param2].to_i
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-	get '/home' => 'home#home'
+	get '' => 'home#home'
+	get '/plus/:param1/:param2' => 'home#plus'
+	get '/calc' => 'home#calc'
+	post 'result' => 'home#result'
       	# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
