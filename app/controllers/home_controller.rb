b50188db50fd8d43e0de1b4432e8a6485c6c477e
wheel0012 <wheel0012@outlook.kr>
2019-07-31 17:39:07 +0000

    add UD in CRUD

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
 	def home
+		@post = Post.all
 	end
 	def calc
 		
@@ -9,5 +10,32 @@ class HomeController < ApplicationController
 	end
 	def result
 		@result = params[:param1].to_i + params[:param2].to_i
+	end
+	def write
+		
+	end
+	def create
+		post = Post.new
+		post.title = params[:title]
+		post.content = params[:content]
+		post.save
+		
+		redirect_to '/'
+	end
+	def update
+		post = Post.find(params[:post_id])
+		post.title = params[:title]
+		post.content = params[:content]
+		post.save
+		redirect_to '/'
+	end
+	def delete
+		post = Post.find(params[:post_id])
+		post.delete
+		redirect_to '/'
+	end
+	def modify
+		@post = Post.find(params[:post_id])
+		@post_id = @post.id
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
 	get '/plus/:param1/:param2' => 'home#plus'
 	get '/calc' => 'home#calc'
 	post 'result' => 'home#result'
+	get '/write' => 'home#write'
+	post '/create' => 'home#create'
+	get '/delete/:post_id' => 'home#delete'
+	post '/update/:post_id' => 'home#update'
+	get '/modify/:post_id' => 'home#modify'
       	# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
